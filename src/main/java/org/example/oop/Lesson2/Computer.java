1518af772e50e169f69e24efd7cf90740768b9ba
Anne <savina.anna15u@gmail.com>
2025-09-28 23:48:16 +0500

    Lesson 3 Конструкторы

--- src/main/java/org/example/oop/Lesson2/Computer.java
+++ src/main/java/org/example/oop/Lesson2/Computer.java
@@ -2,13 +2,34 @@ package org.example.oop.Lesson2;
 
 public class Computer {
 
-    int ssd = 500;
-    int ram = 1024;
+    private int ssd = 500;
+    private int ram;
 
-    void load() {
+    Computer() { // объявлен констр
+        System.out.println("Я был создан");
+    }
+
+    Computer(int newSsd) { //аргрумент метода в ()
+        ssd = newSsd;
+    }
+
+    Computer(int newRam, int newSSD) {
+        ram = newRam;
+        ssd = newSSD;
+    }
+
+    /* Computer(String a) {
+    //}
+    //  Computer(boolean b) {
+    //  }
+       21 и 25 перегруз методов с одинак названием,
+  */
+    public void load() {
         System.out.println("Я загрузился");
     }
-    void printStatec(){
-        load();
+
+    public void printState() {
+        System.out.println("Ssd: " + ssd);
+        System.out.println("Ram: " + ram);
     }
 }
--- src/main/java/org/example/oop/Lesson2/ComputerRunner.java
+++ src/main/java/org/example/oop/Lesson2/ComputerRunner.java
@@ -3,9 +3,22 @@ package org.example.oop.Lesson2;
 public class ComputerRunner {
     public static void main(String[] args) {
         int x = 5;
-        Computer computer = new Computer(); // new создание нового объекта
+        // конструктор без параметров / дефолтный конструктор
+        Computer computer = new Computer(); // new создание нового объекта/ экземляр класса комп
+        // comp comp = это присваивание
+
         computer.load();
-        System.out.println("Ram: " + computer.ram);
-        System.out.println("SSD: " + computer.ssd);
+        computer.printState();
+
+      //  Computer computer3 = new Computer(12, 7);
+        Computer computer2 = new Computer(1050);
+        computer2.printState();
+        Computer computer3 = new Computer(1050, 700);
+        computer3.printState();
     }
 }
+/* Правило перегрузки метода - изменение сигнатуры метода.
+Сигнатура метода: количесвто аргументов метода, тип аргументов и порядок аргументов.
+
+Тип возвращаемого значения метода не является частью сигнатуры метода, следовательно изменение только типа возвращаемого значения метода не является перегрузкой метода.
+ */
