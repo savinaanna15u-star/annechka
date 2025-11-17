c5e775b21d54e89a9e9ed486365b30358c65580a
Anne <savina.anna15u@gmail.com>
2025-11-18 00:54:44 +0500

    внутренние и вложенные классы

--- src/main/java/org/example/lesson15/Archer.java
+++ src/main/java/org/example/lesson15/Archer.java
@@ -2,13 +2,35 @@ package org.example.lesson15;
 
 public class Archer extends Hero {
 
+    private Wolf wolf;
+
     public Archer(String name, int damage){
         super(name, damage);
+        this.wolf = new Wolf( "Wolf", 7);
     }
 
     @Override
     public void attackEnemy(Enemy enemy) {
+
         System.out.println(getName() + " стрельнул из лука в " + enemy.getName());
-        enemy.takeDamage(getDamage());
+        wolf.attaсkEnemy(enemy);
     }
+
+    public class Wolf { // private class только если внутренний класс (если static то вложенный)
+        private String name;
+        private int damage;
+
+        public Wolf(String name, int damage) {
+            this.name = name;
+            this.damage = damage;
+        }
+
+        public void attaсkEnemy(Enemy enemy){
+            System.out.println(name + " и " + Archer.this.getName() + " наносят совместный урон");
+            enemy.takeDamage(damage + Archer.this.getDamage());
+
+        }
+    }
+
 }
+// во внутреннем классе можно обращаться к объекту класса Archer
--- src/main/java/org/example/lesson15/TraningGround.java
+++ src/main/java/org/example/lesson15/TraningGround.java
@@ -5,7 +5,9 @@ public class TraningGround {
     public static void main(String[] args) {
         Hero warrior = new Warrior("Боромир", 15);
         Hero mage = new Mage("Гендольф", 20);
-        Hero archer = new Archer("Леголас", 10);
+        Archer archer = new Archer("Леголас", 10);
+
+        Archer.Wolf wolf = archer.new Wolf("Second Wolf", 11);
 
         Enemy enemy = new Enemy("Зомби", 100);
         attackEnemy(enemy, warrior, mage, archer);
